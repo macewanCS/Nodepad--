76fbc8b7723b90ab107162e3a048f7a54a09501d
Brett A.T. Anderson <baanderson237@gmail.com>
2017-02-14 11:16:49 -0800

    add db.js & some testing

--- src/app/server.js
+++ src/app/server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-
+var mysql = require('mysql');
 var app = express();
 
 app.set('view engine', 'ejs');
---
+++ src/db.js
@@ -0,0 +1,31 @@
+var mysql = require('mysql');
+var connection  =  mysql.createConnection({
+	host : 'localhost',
+	user : 'Brett',
+	password : '123',
+	database : 'test'
+});
+
+connection.connect();
+
+connection.query('SELECT * from users',
+	function(err, rows, fields){
+		if(!err){
+			var test = rows[1].username;
+			console.log('The solution is: ', test);
+			return test;
+		}	
+		else
+			console.log('Error');
+		return "we fucked up";
+	});
+
+/*connection.query('Select 1+ 1 AS solution',
+	function(err,rows,fields) {
+		if (err){
+			console.log('We fucked up');
+		}
+
+		console.log('the solution is', rows[0].solution);
+	})*/
+connection.end();
