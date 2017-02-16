90893dcf5f4e769ae3fc8297c5f0e0f2bd56f009
JackStaples <staplesj3@mymacewan.ca>
2017-02-15 21:54:04 -0700

    base html skeleton onto forms

--- src/app/controllers/ticket_creation_controller.js
+++ src/app/controllers/ticket_creation_controller.js
@@ -3,24 +3,24 @@ exports.categories = function(req, res){
 };
 
 exports.forms = function(req, res){
-  console.log("started looking with " + req.params.ticketid);
-  if (req.params.ticketid = /hardware/){
+  console.log("started looking with " + req.params.form);
+  if (req.params.form.match(/hardware/i)){
     console.log("Its a hardware ticket, eh");
     res.render((__dirname + '/../../public/views/hardware.ejs'));
   }
-  else if (req.params.ticketid = /software/){
+  else if (req.params.form.match(/software/i)){
     console.log("Its a software ticket, eh");
     res.render((__dirname + '/../../public/views/software.ejs'));
   }
-  else if (req.params.ticketid = /software/){
+  else if (req.params.form.match(/password/i)){
     console.log("Its a password ticket, eh");
     res.render((__dirname + '/../../public/views/password.ejs'));
   }
-  else if (req.params.ticketid = /software/){
+  else if (req.params.form.match(/service/i)){
     console.log("Its a service ticket, eh");
     res.render((__dirname + '/../../public/views/service.ejs'));
   }
-  else if (req.params.ticketid = /software/){
+  else if (req.params.form.match(/general/i)){
     console.log("Its a general ticket, eh");
     res.render((__dirname + '/../../public/views/general.ejs'));
   }
