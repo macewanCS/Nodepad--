dcfc93bb9c8c02b9e92d5855ab806e5d5c266ab7
JackStaples <staplesj3@mymacewan.ca>
2017-02-19 15:15:40 -0700

    Delete files

--- src/app/server.js
+++ src/app/server.js
@@ -1,14 +1,27 @@
 var express = require('express');
 var bodyParser = require("body-parser");
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var app = express();
+
+var passport = require('passport');
+
+require("../config/passport")(passport);
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
-
+app.use(cookieParser());
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/../public/views');
-
-var routes =require("./routes.js")(app);
+app.use(session({
+	secret: 'vidyapathaisalwaysrunning',
+	resave: true,
+	saveUninitialized: true
+} )); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+var routes =require("./routes.js")(app, passport);
 
 app.use(express.static(__dirname + '/../public'));
 
