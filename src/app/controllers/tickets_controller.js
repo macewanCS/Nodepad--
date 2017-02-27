14157de9e887b678a53f88ad77736280882b7770
JackStaples <staplesj3@mymacewan.ca>
2017-02-27 13:38:29 -0700

    closed tickets pulling from database

--- src/app/controllers/tickets_controller.js
+++ src/app/controllers/tickets_controller.js
@@ -1,3 +1,5 @@
+var mysql = require('mysql');
+
 exports.view = function(req,res){
   res.send("You entered " + req.params.ticketid + " right? if it isnt please tell Jack and he can fix it and then things will work and he wont be sad.");
 };
@@ -10,6 +12,25 @@ exports.resolve = function(req,res){
   res.send("Congrats, you've found the secret page where ticket resolving will go." + req.params.ticketid + " Sick number bro.");
 };
 
-exports.mytickets = function(req,res){
-  res.render((__dirname + '/../../public/views/mytickets.ejs'));
-};
+exports.mytickets = function(req, res){
+  
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '1234',
+    database: '395project'
+  });
+  
+  connection.connect(function(err) {
+    if (err) throw err
+      console.log('You are now connected...')
+      connection.query('SELECT Category, CallStatus, Symptoms FROM 395project.calllog WHERE CustID="' + req.user.id + '" and CallStatus="Closed";', function(err, result) {
+        if (err) throw err
+          connection.end();
+            var myVar = JSON.stringify(result);
+            res.render((__dirname + '/../../public/views/mytickets.ejs'), {
+            data:myVar
+            });
+    });
+  });
+}
--- src/app/server.js
+++ src/app/server.js
@@ -2,8 +2,8 @@ var express = require('express');
 var bodyParser = require("body-parser");
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
-var app = express();
 
+var app = express();
 var passport = require('passport');
 
 require("../config/passport")(passport);
