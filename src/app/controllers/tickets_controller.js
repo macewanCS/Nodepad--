a10c0ac9e709287c3102975783285fd06e6045cf
JackStaples <staplesj3@mymacewan.ca>
2017-02-27 18:49:53 -0700

    tweaks to pulling, visual only

--- src/app/controllers/tickets_controller.js
+++ src/app/controllers/tickets_controller.js
@@ -22,15 +22,37 @@ exports.mytickets = function(req, res){
   });
   
   connection.connect(function(err) {
+    
+    var myVar, myVar2, myVar3;
     if (err) throw err
       console.log('You are now connected...')
+      
       connection.query('SELECT Category, CallStatus, Symptoms FROM 395project.calllog WHERE CustID="' + req.user.id + '" and CallStatus="Closed";', function(err, result) {
         if (err) throw err
-          connection.end();
-            var myVar = JSON.stringify(result);
+            myVar = JSON.stringify(result);
+            console.log(myVar);
+    });
+    console.log('closed');
+    if (err) throw err
+      connection.query('SELECT Category, CallStatus, Symptoms FROM 395project.calllog WHERE CustID="' + req.user.id + '" and CallStatus="Open";', function(err, result) {
+        if (err) throw err
+            myVar2 = JSON.stringify(result);
+            console.log(myVar2);
+    });
+    console.log('open');
+    
+    if (err) throw err
+      connection.query('SELECT Category, CallStatus, Symptoms FROM 395project.calllog WHERE CustID="' + req.user.id + '";', function(err, result) {
+        if (err) throw err
+            myVar3 = JSON.stringify(result);
+            console.log(myVar3);
             res.render((__dirname + '/../../public/views/mytickets.ejs'), {
-            data:myVar
+            allTickets:myVar3,
+            openTickets:myVar2,
+            closedTickets:myVar,
+            
             });
     });
+    console.log('all');
   });
 }
