6aa1ec3dcf7855475a32ac039730ddd8e845c287
JackStaples <staplesj3@mymacewan.ca>
2017-03-12 15:39:16 -0600

    service.ejs now posting into server:

--- src/app/controllers/ticket_creation_controller.js
+++ src/app/controllers/ticket_creation_controller.js
@@ -38,6 +38,35 @@ exports.forms = function(req, res){
 }
 exports.service = function(req, res){
   console.log("create service is running");
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '1234',
+    database: '395project'
+  });
+  
+  console.log(req.body.EquipmentType);
+  
+  connection.connect(function(err) {
+    
+    console.log("Beginning insertion");
+    
+    var today = new Date();
+    var stringDate = today.getFullYear() + "/" + (parseInt(today.getMonth()) + 1) + "/" + today.getDate();
+    
+    console.log('INSERT INTO `395project`.`calllog`(`Symptoms`, `Priority`, `CallSource`, `RecvdDate`, `RecvdTime`, `CustID`, `Tracker`, `CallStatus`, `Category`, `CustType`) VALUES ("' 
+    + req.body.EquipmentType + ", " + req.body.System + ", " + req.body.AssetTag + ", " + req.body.Location + ", " +req.body.SoftwareName + " " +
+    '", "3", "Web", "' + today.toLocaleDateString() + '", "' + today.toTimeString().slice(0,8) + '", "' + req.user.id + '", "selfserve" , "Open", "Service", "Employee")');
+    
+    connection.query('INSERT INTO `395project`.`calllog`(`Symptoms`, `Priority`, `CallSource`, `RecvdDate`, `RecvdTime`, `CustID`, `Tracker`, `CallStatus`, `Category`, `CustType`) VALUES ("' 
+    + req.body.EquipmentType + ", " + req.body.System + ", " + req.body.AssetTag + ", " + req.body.Location + ", " +req.body.SoftwareName + " " +
+    '", "3", "Web", "' + today.toLocaleDateString() + '", "' + today.toTimeString().slice(0,8) + '", "' + req.user.id + '", "selfserve" , "Open", "Service", "Employee")');
+    
+    //connection.query('INSERT INTO `395project`.`asgnmnt`(`Description`, `TeamName`, `AssignedBy`, `Status`, `DateAssign`, `TimeAssign`) VALUES ("' + Concat info '", + "Help Desk Team", "Selfserve", "Unacknowledged", "' + CURRENT DATE '", "' + CURRENT TIME + '"'););
+    
+    console.log("Ending insertion, check the database to confirm");
+    connection.end();
+  });
 }
 exports.hardware = function(req, res){
   console.log("Create hardware is running");
