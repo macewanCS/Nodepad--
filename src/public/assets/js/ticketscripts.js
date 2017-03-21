0101c5721dfd806f32b5e415e6a926cd8b63c2b2
JackStaples <staplesj3@mymacewan.ca>
2017-03-20 21:22:43 -0600

    javascript merging

---
+++ src/public/assets/js/ticketscripts.js
@@ -0,0 +1,31 @@
+
+
+
+function colorHeader(header, change){
+  $(change).css('visibility', 'visible');
+  $(header).css('background-color', 'rgb(170,170,170)');
+};
+
+function toggleAccordian(close, open){
+  $(close).collapse("toggle");
+  $(open).collapse("toggle");
+};
+
+function toggleAccordianCheck(close, open, mandatory){
+  if ($(mandatory).val().length > 0){
+    console.log($(mandatory).val().length || !$(mandatory).val().match(/'s/));
+    $(close).collapse("toggle");
+    $(open).collapse("toggle");
+  } else {
+  console.log($(mandatory).val());
+    alert("Please fill it out the information before proceeding")
+  }
+};
+
+function undo(open, close1, close2, close3, close4){
+    $(close1).collapse("hide");
+    $(close2).collapse("hide");
+    $(close3).collapse("hide");
+    $(close4).collapse("hide");
+    $(open).collapse("show");
+};
